59b2ce281a36968bdf6b77a4178d704923f9d4f8
Patrick Adams <padams@tonicdesign.com>
2015-11-24 09:10:37 -0500

    Updated UI

--- StyleKit-Demo/Style/Style.swift
+++ StyleKit-Demo/Style/Style.swift
@@ -1,10 +1,8 @@
-//Theme Generated:2015-11-16 15:54:50
-
 import UIKit
 
-class Theme: NSObject {
+class Style: NSObject {
 
-    static let sharedInstance = Theme()    
+    static let sharedInstance = Style()    
     
     let primaryFontMedium: String = "BrandonGrotesque-Medium"    
     let primaryFontBlack: String = "BrandonGrotesque-Black"    
@@ -12,9 +10,9 @@ class Theme: NSObject {
     let primaryFontLightItalic: String = "BrandonGrotesque-LightItalic"    
     let primaryFontBold: String = "BrandonGrotesque-Bold"    
     
-    let purpleColor = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)    
-    let whiteColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)    
-    let blackColor = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)    
+    let blueColor = UIColor(red: 0.0/255.0, green: 84.0/255.0, blue: 136.0/255.0, alpha: 1.0)    
+    let whiteColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)    
+    let blackColor = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)    
     
     let buttonImage1 = UIImage(named: "black_button_image")    
     
@@ -33,6 +31,12 @@ class Theme: NSObject {
     @IBOutlet var H1Label: [UILabel]! {        
         didSet {            
             styleH1Label(H1Label)            
+        }        
+    }    
+    
+    @IBOutlet var specialView: [UIView]! {        
+        didSet {            
+            stylespecialView(specialView)            
         }        
     }    
     
@@ -68,66 +72,89 @@ class Theme: NSObject {
     
     func styleH2Label(objects: [UILabel]) {        
         for object in objects {        
-            object.font = UIFont (name: primaryFontLightItalic, size: 20)            
             object.textColor = blackColor            
+            object.font = UIFont (name: primaryFontLightItalic, size: 20)            
             }            
         }    
     
     func styleH3Label(objects: [UILabel]) {        
         for object in objects {        
+            object.textColor = blueColor            
             object.font = UIFont (name: primaryFontBlack, size: 24)            
             }            
         }    
     
     func styleH1Label(objects: [UILabel]) {        
         for object in objects {        
-            object.font = UIFont (name: primaryFontLight, size: 34)            
             object.textColor = whiteColor            
+            object.font = UIFont (name: primaryFontLight, size: 34)            
+            object.textAlignment = NSTextAlignment.Center            
+            }            
+        }    
+    
+    func stylespecialView(objects: [UIView]) {        
+        for object in objects {        
+            object.backgroundColor = blueColor            
+            object.layer.cornerRadius = 5            
+            object.layer.borderColor = blackColor.CGColor            
+            object.layer.borderWidth = 5            
             }            
         }    
     
     func styleB4Button(objects: [UIButton]) {        
         for object in objects {        
-            object.setTitleColor(blackColor, forState: .Normal)            
-            object.titleLabel?.font = UIFont (name: primaryFontBlack, size: 26)            
+            object.backgroundColor = whiteColor            
             object.layer.borderColor = blackColor.CGColor            
-            object.layer.borderWidth = 1            
+            object.layer.borderWidth = 3            
+            object.setTitleColor(blackColor, forState: .Normal)            
+            object.titleLabel?.font = UIFont (name: primaryFontBold, size: 17)            
             }            
         }    
     
     func styleB1Button(objects: [UIButton]) {        
         for object in objects {        
             object.backgroundColor = blackColor            
+            object.layer.cornerRadius = 10            
+            object.layer.borderColor = whiteColor.CGColor            
+            object.layer.borderWidth = 3            
             object.setTitleColor(whiteColor, forState: .Normal)            
-            object.titleLabel?.font = UIFont (name: primaryFontLight, size: 20)            
-            object.layer.cornerRadius = 15            
+            object.setTitleColor(blueColor, forState: .Highlighted)            
+            object.titleLabel?.font = UIFont (name: primaryFontLight, size: 34)            
             }            
         }    
     
     func styleB2Button(objects: [UIButton]) {        
         for object in objects {        
             object.backgroundColor = whiteColor            
-            object.setTitleColor(blackColor, forState: .Normal)            
-            object.titleLabel?.font = UIFont (name: primaryFontMedium, size: 22)            
-            object.layer.cornerRadius = 10            
+            object.layer.cornerRadius = 3            
+            object.layer.borderColor = blueColor.CGColor            
+            object.layer.borderWidth = 5            
+            object.setTitleColor(blueColor, forState: .Normal)            
+            object.titleLabel?.font = UIFont (name: primaryFontLight, size: 28)            
             }            
         }    
     
     func styleB3Button(objects: [UIButton]) {        
         for object in objects {        
-            object.setTitleColor(whiteColor, forState: .Normal)            
-            object.titleLabel?.font = UIFont (name: primaryFontBold, size: 24)            
-            object.layer.cornerRadius = 5            
-            object.layer.borderColor = whiteColor.CGColor            
-            object.layer.borderWidth = 5            
+            object.backgroundColor = blackColor            
+            object.layer.cornerRadius = 15            
+            object.layer.borderColor = blueColor.CGColor            
+            object.layer.borderWidth = 3            
+            object.setTitleColor(blueColor, forState: .Normal)            
+            object.titleLabel?.font = UIFont (name: primaryFontMedium, size: 20)            
             }            
         }    
     
     func styleT1TextField(objects: [UITextField]) {        
         for object in objects {        
-            object.textColor = blackColor            
-            object.backgroundColor = whiteColor            
+            object.backgroundColor = blueColor            
+            object.layer.cornerRadius = 5            
             object.layer.borderColor = blackColor.CGColor            
+            object.layer.borderWidth = 2            
+            object.textColor = whiteColor            
+            object.font = UIFont (name: primaryFontMedium, size: 15)            
+            object.textAlignment = NSTextAlignment.Center            
+            object.borderStyle = UITextBorderStyle.RoundedRect            
             }            
         }    
     
--- StyleKit-Demo/TableViewCellTest1.swift
+++ StyleKit-Demo/TableViewCellTest1.swift
@@ -3,14 +3,14 @@ import UIKit
 
 class TableViewCell1: UITableViewCell {
     
-    let theme = Theme.sharedInstance
+//    let theme = Theme.sharedInstance
     
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet weak var buttonView: UIImageView!
     
     override func awakeFromNib() {
-        theme.styleH2Label([self.titleLabel])
-        buttonView.image = theme.buttonImage1
+//        theme.styleH2Label([self.titleLabel])
+//        buttonView.image = theme.buttonImage1
     }
     
 }
