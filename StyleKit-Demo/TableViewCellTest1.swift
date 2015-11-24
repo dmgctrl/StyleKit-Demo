56e462dda979ccc153a4d22c9511845e40255edc
Patrick Adams <padams@tonicdesign.com>
2015-11-24 09:33:09 -0500

    Updated the cell to use style not theme

--- StyleKit-Demo/TableViewCellTest1.swift
+++ StyleKit-Demo/TableViewCellTest1.swift
@@ -3,14 +3,14 @@ import UIKit
 
 class TableViewCell1: UITableViewCell {
     
-//    let theme = Theme.sharedInstance
+    let style = Style.sharedInstance
     
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet weak var buttonView: UIImageView!
     
     override func awakeFromNib() {
-//        theme.styleH2Label([self.titleLabel])
-//        buttonView.image = theme.buttonImage1
+        style.styleH2Label([self.titleLabel])
+        buttonView.image = style.buttonImage1
     }
     
 }
