096eaa0efffb7efe52878ae8f74d03dc7c4be55b
Eric Kille <ekille@tonicdesign.com>
2016-10-24 09:03:15 -0400

    Adding tableview and cells for StyleViewController

---
+++ NoPythonDemo/StyleKitSample/ProgressViewsTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ProgressViewsTableViewCell.swift
+//  StyleKitSample
+//
+//  Created by Eric Kille on 10/22/16.
+//  Copyright © 2016 Tonic Design. All rights reserved.
+//
+
+import UIKit
+
+class ProgressViewsTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
---
+++ NoPythonDemo/StyleKitSample/StylesViewController.swift
@@ -0,0 +1,98 @@
+
+import UIKit
+
+class StylesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Stylist"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+    var sectionHeaders = Array(Style.sharedInstance.styleMap.keys)
+    var styleMap = Style.sharedInstance.styleMap
+    var resources:[String:AnyObject] = ["Colors":Array(Style.sharedInstance.resources.colors.keys),
+                                        "Fonts":Array(Style.sharedInstance.resources.fontLabels.keys),
+                                        "Images":Array(Style.sharedInstance.resources.imageNames.keys)]
+
+}
+
+
+extension StylesViewController: UITableViewDelegate {
+
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        switch sectionHeaders[indexPath.section] {
+        case .button, .view, .textField:
+            return 70.0
+        default:
+            return 54.0
+        }
+    }
+}
+
+extension StylesViewController: UITableViewDataSource {
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let styles = styleMap[sectionHeaders[section]] {
+            return styles.keys.count
+        }
+        return 0
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let sectionHeader = sectionHeaders[indexPath.section]
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("\(sectionHeader.rawValue)Cell", forIndexPath: indexPath)
+
+        var styleTag = ""
+        if let styles = styleMap[sectionHeader] {
+            let allTags = Array(styles.keys)
+            styleTag = allTags[indexPath.row]
+            
+            switch sectionHeader {
+            case .button:
+                (cell as! ButtonTableViewCell).button.styleTag = styleTag
+                (cell as! ButtonTableViewCell).button.setTitle(styleTag, forState: .Normal)
+            case .segmentedControl:
+                (cell as! SegmentedControlsTableViewCell).segmentedControl.styleTag = styleTag
+            case .textField:
+                (cell as! TextFieldsTableViewCell).textField.styleTag = styleTag
+                (cell as! TextFieldsTableViewCell).textField.text = styleTag
+            case .label:
+                (cell as! LabelsTableViewCell).label.styleTag = styleTag
+                (cell as! LabelsTableViewCell).label.text = styleTag
+            case .slider:
+                (cell as! SlidersTableViewCell).slider.styleTag = styleTag
+            case .stepper:
+                (cell as! SteppersTableViewCell).stepper.styleTag = styleTag
+            case .progressView:
+                (cell as! ProgressViewsTableViewCell).progressView.styleTag = styleTag
+            case .view:
+                (cell as! ViewsTableViewCell).view.styleTag = styleTag
+                (cell as! ViewsTableViewCell).label.text = styleTag
+            }
+        }
+                
+        return cell
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return sectionHeaders.count
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionHeaders[section].rawValue
+    }
+    
+    
+    
+}
